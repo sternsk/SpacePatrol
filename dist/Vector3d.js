bf2c1824cb275c27e8dc9523a1798898a29c9dea
sternsk <sternski@gmail.com>
2024-05-21 18:08:55 +0200

    added tone.js

--- dist/Vector3d.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-class Vector3d {
-    _x = 0;
-    _y = 0;
-    _z = 0;
-    constructor(x, y, z) {
-    }
-}
--- dist/library.js
+++
@@ -1,6 +0,0 @@
-import { SpaceGame } from "./SpaceGame";
-export function initGame(gameFrame, type, color, id) {
-    console.log("spaceGame loads");
-    const game = new SpaceGame();
-    game.init(type, color, id);
-}
